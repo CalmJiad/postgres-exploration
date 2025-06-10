e965b9f4d5bf1ac618ad27d49b0755820bc4bb8c
calmjiad <unlucky4246@gmail.com>
2025-06-10 13:16:29 +0200

    restructured the fk comments des

--- foreign_key.sql
+++ foreign_key.sql
@@ -1,73 +1,82 @@
-
+-- Create a table called 'randomuser'
+-- 'id' will be created automatically and uniquely for each user
+-- 'username' must be provided (cannot be empty)
 CREATE TABLE randomuser (
-    id SERIAL PRIMARY KEY,               -- Automatically generated unique ID for each user
+    id SERIAL PRIMARY KEY,               
     username VARCHAR(25) NOT NULL        
 );
 
--- Create a table named 'posts'
--- This table will store blog posts or any content written by users
--- 'id' is the primary key (unique identifier for each post)
--- 'title' is a required text field for the post title/content
--- 'user_id' is a foreign key that links each post to a user in the 'randomuser' table
-
+-- Create a table called 'posts'
+-- This table will store posts written by users
+-- 'id' is the unique post ID
+-- 'title' is the content of the post and must not be empty
+-- 'user_id' links the post to a user from the 'randomuser' table
 CREATE TABLE posts (
     id SERIAL PRIMARY KEY,                             
     title TEXT NOT NULL,                               
-    user_id INTEGER REFERENCES randomuser(id)          -- Foreign key constraint
+    user_id INTEGER REFERENCES randomuser(id)          
 );
 
+-- Make sure 'user_id' in posts table is required (cannot be empty)
 ALTER table posts
-    alter COLUMN user_id set NOT NULL
+    alter COLUMN user_id set NOT NULL;
 
-
+-- Add 5 users to the 'randomuser' table
 INSERT INTO randomuser(username) VALUES
 ('Rocky'),
 ('Robin'),
 ('Munshi'),
 ('Iqbal'),
-('Khurshed')
+('Khurshed');
 
-SELECT * from randomuser
+-- Show all users
+SELECT * from randomuser;
 
-SELECT * from posts
+-- Show all posts
+SELECT * from posts;
 
+-- Add 4 posts, each linked to a user by user_id
 INSERT INTO posts(title, user_id) VALUES
 ('Enjoying a sunny day today', 1),
 ('It might be not that much easy', 3),
 ('The sun is shining', 2),
-('Apple has announced its latest OS', 5)
-('Germany had its election last month', 4)
--- Deletion constraints regarding foreign key
+('Apple has announced its latest OS', 5),
+('Germany had its election last month', 4);
 
--- On delete restriction / on delete no action (default)
+-- Try to delete a user with ID = 4
+-- This may fail if there are posts linked to this user
+-- Because by default, deletion of a user is not allowed if they have posts
 DELETE FROM randomuser
-    WHERE id = 4
+    WHERE id = 4;
 
--- On delete cascade
-DROP TABLE randomuser
+-- Now we drop (delete) both tables completely
+DROP TABLE randomuser;
+DROP TABLE posts;
 
-DROP TABLE posts
-
--- recreating the tables
+-- Recreate the 'randomuser' table
 CREATE TABLE randomuser (
-    id SERIAL PRIMARY KEY,               -- Automatically generated unique ID for each user
+    id SERIAL PRIMARY KEY,               
     username VARCHAR(25) NOT NULL        
 );
 
--- Now posts table with new deletion constraints
+-- Recreate the 'posts' table with a new rule:
+-- If a user is deleted, all their posts will also be deleted automatically
 CREATE TABLE posts (
     id SERIAL PRIMARY KEY,                             
     title TEXT NOT NULL,                               
-    user_id INTEGER REFERENCES randomuser(id) ON DELETE CASCADE         -- Foreign key constraint
+    user_id INTEGER REFERENCES randomuser(id) ON DELETE CASCADE
 );
 
+-- Delete the user with ID = 4
+-- Their posts (if any) will be deleted automatically because of the cascade rule
 DELETE FROM randomuser
-    WHERE ID = 4
+    WHERE ID = 4;
 
+-- Delete the user with ID = 3
+-- Their posts (if any) will also be deleted
 DELETE FROM randomuser
-    WHERE ID = 3
+    WHERE ID = 3;
 
-
--- There are many other deletion constraints could be set while Deletion process
--- 1. ON DELTE SET NULL
--- 2. ON DELETE SET DEFAULT
+-- Other delete options you can use instead of CASCADE:
+-- 1. ON DELETE SET NULL → sets user_id in posts to NULL when the user is deleted
+-- 2. ON DELETE SET DEFAULT → sets user_id to a default value when the user is deleted
